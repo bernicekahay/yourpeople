2b64dccd95b343761553045638f7f00cf94d9f05
Gavin Mak <gavinmak98@gmail.com>
2016-12-08 09:59:12 -0800

    finalize temp mission page

--- js/mission.js
+++ js/mission.js
@@ -1,14 +1,19 @@
+//FORMAT TBD, THIS IS TEMP
+
 $(function() {
+  //helper variables
     var insideWe = true;
     var insideProblem = true;
     var insideSolution = true;
 
-    var triggerHookText = 0.4;
+    var triggerHookText = 0.5;
     var triggerHookMessage = 0.4;
     var parDuration = 0;
 
+    //ScrollMagic controller
     var controller = new ScrollMagic.Controller();
 
+    //pin top words
     var stickTop = new ScrollMagic.Scene({
         triggerElement: "body",
         triggerHook: 0
@@ -16,10 +21,13 @@ $(function() {
     .setPin("#message-top")
     .addTo(controller);
 
-    var stickWe = new ScrollMagic.Scene({
+    //first group of words, "we"
+    var We = new ScrollMagic.Scene({
         triggerElement: "div#we.message",
-        triggerHook: triggerHookText
+        triggerHook: triggerHookText,
+        offset: 50
     })
+    //fade out unimportant words and keep important words when scroll past trigger
     .on("start", function(e) {
         if(insideWe) {
             $("div#we.fade").velocity({opacity: 0}, {duration: 200});
@@ -34,6 +42,7 @@ $(function() {
     })
     .addTo(controller);
 
+    //pin important words to top and edit margins
     var stickWeImportant = new ScrollMagic.Scene({
         triggerElement: "div#we.important",
         triggerHook: triggerHookMessage
@@ -47,8 +56,8 @@ $(function() {
     .setPin("div#we.important")
     .addTo(controller);
 
-
-    var stickProblem = new ScrollMagic.Scene({
+    //second group of words, "problem"
+    var Problem = new ScrollMagic.Scene({
         triggerElement: "div#problem.message",
         triggerHook: triggerHookText
     })
@@ -66,28 +75,29 @@ $(function() {
     })
     .addTo(controller);
 
+    //fade out unimportant words and keeps important ones
     var stickProblemImportant = new ScrollMagic.Scene({
         triggerElement: "div#problem.important",
         triggerHook: triggerHookMessage
     })
     .on("enter", function () {
-      console.log("helo enetere");
-
       $("div#we.important").text("Lorem ipsum dolor sit amet, consectetur \
-      adipiscing elit. Proin dignissim urna sed lorem aliquet, sed tincidunt \
-      tortor dignissim. Curabitur semper tempor cursus. Mauris sed consectetur est,");
+      adipiscing elit.");
+
+      //to prevent skipping the changing opacity
+      $("div#problem.important").text("");
       $("div#problem.important").css("opacity", 0);
-      //$("div#problem.message-text").css("opacity", 0.5);
     })
     .on("leave", function () {
-      console.log("helo asdf");
       $("div#we.important").text("Lorem ipsum dolor");
+      $("div#problem.important").text(" sit amet, consectetur \
+      adipiscing elit.");
       $("div#problem.important").css("opacity", 0.5);
-
     })
     .addTo(controller);
 
-    var stickSolution = new ScrollMagic.Scene({
+    //pins important words from solution
+    var Solution = new ScrollMagic.Scene({
         triggerElement: "div#solution.message",
         triggerHook: triggerHookText,
         duration: parDuration
@@ -106,24 +116,26 @@ $(function() {
     })
     .addTo(controller);
 
+    //important words stay, when reach top, make faded text opacity 1
     var stickSolutionImportant = new ScrollMagic.Scene({
         triggerElement: "div#solution.important",
         triggerHook: triggerHookMessage
     })
     .on("enter", function () {
       $("div#we.important").velocity({opacity: 1}, {duration: 200});
-      $("div#solution.important").css("opacity", 0);
       $("div#we.important").text("Lorem ipsum dolor sit amet, consectetur \
       adipiscing elit. Proin dignissim urna sed lorem aliquet, sed tincidunt \
-      tortor dignissim. Curabitur semper tempor cursus. Mauris sed consectetur est, \
-      asfasdf asdf adipiscing elit.");
+      tortor dignissim.");
+
+      $("div#solution.important").css("opacity", 0);
+      $("div#solution.important").text("");
     })
     .on("leave", function () {
       $("div#we.important").velocity({opacity: 0.5}, {duration: 200});
+      $("div#we.important").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+
+      $("div#solution.important").text("Proin dignissim urna sed lorem aliquet, sed tincidunt tortor dignissim.");
       $("div#solution.important").css("opacity", 0.5);
-      $("div#we.important").text("Lorem ipsum dolor sit amet, consectetur \
-      adipiscing elit. Proin dignissim urna sed lorem aliquet, sed tincidunt \
-      tortor dignissim. Curabitur semper tempor cursus. Mauris sed consectetur est,");
     })
     .addTo(controller);
 });
--- mission.php
+++ mission.php
@@ -7,7 +7,7 @@ require_once("css/header.php");
 
      <div class="container text-center bring-down-more" id="message-top">
         <h2 class="title-text" id="top-text">
-            Our Mission
+            Mission Top Text
         </h2>
     </div>
 
@@ -24,7 +24,7 @@ require_once("css/header.php");
     </div> -->
 
     <div class="message" id="we">
-        <h2 class="title-text" id="we">We</h2>
+        <h2 class="title-text" id="we">title one</h2>
         <div class="message-text" id="we">
             <div class="important" id="we">Lorem ipsum dolor</div>
             <div class="fade" id="we"> sit amet, consectetur adipiscing elit. Proin dignissim urna sed lorem aliquet, sed tincidunt tortor dignissim.
@@ -35,33 +35,32 @@ require_once("css/header.php");
     </div>
 
     <div class="message" id="problem">
-        <h2 class="title-text" id="problem">The Problem</h2>
+        <h2 class="title-text" id="problem">title two</h2>
         <div class="message-text" id="problem">
             <div class="fade" id="problem">Lorem ipsum dolor</div>
-            <div class="important" id="problem"> sit amet, consectetur adipiscing elit. Proin dignissim urna sed lorem aliquet, sed tincidunt tortor dignissim. Curabitur semper tempor cursus.
-                Mauris sed consectetur est,</div>
-            <div class="fade" id="problem"> sed dignissim arcu. Maecenas accumsan nec sapien vel mollis.
+            <div class="important" id="problem"> sit amet, consectetur adipiscing elit.</div>
+            <div class="fade" id="problem"> Proin dignissim urna sed lorem aliquet, sed tincidunt tortor dignissim. Curabitur semper tempor cursus.
+                Mauris sed consectetur est, sed dignissim arcu. Maecenas accumsan nec sapien vel mollis.
                 Nullam sit amet sapien a nisl tristique convallis ut commodo quam.
             </div>
         </div>
     </div>
 
     <div class="message" id="solution">
-        <h2 class="title-text" id="solution">The Solution</h2>
+        <h2 class="title-text" id="solution">title three</h2>
         <div class="message-text" id="solution">
-            <div class="fade" id="solution">Lorem ipsum dolor sit amet, consectetur </div>
-            <div class="important" id="solution">adipiscing elit. </div>
-            <div class="fade" id="solution">Proin dignissim urna sed lorem aliquet, sed tincidunt tortor dignissim.
-                Curabitur semper tempor cursus. Mauris sed consectetur est, sed dignissim arcu.
+            <div class="fade" id="solution">Lorem ipsum dolor sit amet, consectetur adipiscing elit. </div>
+            <div class="important" id="solution">Proin dignissim urna sed lorem aliquet, sed tincidunt tortor dignissim.</div>
+            <div class="fade" id="solution"> Curabitur semper tempor cursus. Mauris sed consectetur est, sed dignissim arcu.
                 Maecenas accumsan nec sapien vel mollis. Nullam sit amet sapien a nisl tristique convallis ut commodo quam.
             </div>
         </div>
     </div>
 
     <div class="message" id="link">
-        <h3 class="title-text">
-            <a href="index.html">To find the solution, click here.</a>
-        </h3>
+        <h2 class="title-text" id="link">
+            <a href="index.html">the solution</a>
+        </h2>
     </div>
 
 <!-- jQuery -->
